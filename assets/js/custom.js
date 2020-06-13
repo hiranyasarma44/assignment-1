7118391c8d9affceac7b0b2167647650d617d18c
Hiranya Sarma <hiranya.sarma44@gmail.com>
2020-06-14 03:03:13 +0530

    finishing responsive design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,17 +1,28 @@
-   $(document).ready(function(){
-    $(window).scroll(function(){
+const navRef = $(".navbar");
+const returnToTop = $('#return-to-top');
+$(document).ready(function () {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        if (scroll > 300) {            
-            $(".navbar").addClass("bg-white");
-            $(".navbar").addClass("shadow");
-            $(".navbar").addClass("pt-2");
-            $(".navbar").removeClass("pt-4");
-        }  
-        else{            
-            $(".navbar").removeClass("bg-white");
-            $(".navbar").removeClass("shadow");
-            $(".navbar").addClass("pt-4");
-            $(".navbar").removeClass("pt-2");
+        if (scroll > 300) {
+            navRef.addClass("bg-white");
+            navRef.addClass("shadow");
+            navRef.addClass("pt-2");
+            navRef.removeClass("pt-4");
+        } else {
+            navRef.removeClass("bg-white");
+            navRef.removeClass("shadow");
+            navRef.addClass("pt-4");
+            navRef.removeClass("pt-2");
+        }
+        if (scroll >= 50) {        // If page is scrolled more than 50px
+            returnToTop.fadeIn(200);    // Fade in the arrow
+        } else {
+            returnToTop.fadeOut(200);   // Else fade out the arrow
         }
     });
-  });
+});
+returnToTop.click(function () {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0                       // Scroll to top of body
+    }, 500);
+});
